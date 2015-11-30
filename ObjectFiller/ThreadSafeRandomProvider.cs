2ca37371200c8128103e74b4dfbd164de49b422d
Benjamin Meyers <blmeyers@users.noreply.github.com>
2015-11-30 14:20:09 -0600

    Switching seed value to cryptographic random seed value per thread, instead of seeding off Environment.TickCount.

--- ObjectFiller/ThreadSafeRandomProvider.cs
+++ ObjectFiller/ThreadSafeRandomProvider.cs
@@ -1,15 +1,29 @@
-﻿using System;
+﻿using System.Security.Cryptography;
 using System.Threading;
 
 namespace Tynamix.ObjectFiller
 {
 	public static class ThreadSafeRandomProvider
 	{
-		private static int _seed = Environment.TickCount;
+		private static readonly int _seed;
 
-		private static readonly ThreadLocal<System.Random> _randomWrapper = new ThreadLocal<System.Random>(() =>
-			new System.Random(Interlocked.Increment(ref _seed))
-		);
+		static ThreadSafeRandomProvider()
+		{
+			_seed = GetRandomSeed();
+		}
+
+		private static int GetRandomSeed()
+		{
+			using (var rngCsp = new RNGCryptoServiceProvider())
+			{
+				var randomNumber = new byte[1];
+				rngCsp.GetBytes(randomNumber);
+				return randomNumber[0];
+			}
+		}
+
+		private static readonly ThreadLocal<System.Random> _randomWrapper =
+			new ThreadLocal<System.Random>(() => new System.Random(GetRandomSeed()));
 
 		public static System.Random GetThreadRandom()
 		{
